d6b0c78e9b4f0c23235e2f55b5858078b43aa106
Kevin Leung <kevinresol@gmail.com>
2017-01-17 16:44:19 +0800

    Guard android only

--- src/react/native/immersive/Immersive.hx
+++ src/react/native/immersive/Immersive.hx
@@ -2,7 +2,9 @@ package react.native.immersive;
 
 @:jsRequire('react-native-immersive', 'default')
 extern class Immersive {
+	#if android
 	public static function on():Void;
 	public static function off():Void;
 	public static function setImmersive(v:Bool):Void;
+	#end
 }
